8a36ddc6772ade4c627144764db4924bafc1f572
PhiVerbiest <220502@student.glu.nl>
2022-11-08 13:09:52 +0100

    commit

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Darkside",
+    artist: "Aversion Unresolved",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "City Lights",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "In Control",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Rave Into Space",
+    artist: "Sub Zero Project",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "One Last Time",
+    artist: "Sub Zero Project",
+    img: "music-5",
+    src: "music-5"
+  }
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
