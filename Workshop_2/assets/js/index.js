95ef3e270debbe947ebd364e1d3e5ff11b408196
PhiVerbiest <220502@student.glu.nl>
2022-11-09 13:45:54 +0100

    yes

---
+++ Workshop_2/assets/js/index.js
@@ -0,0 +1,6 @@
+// function clickFunction() {
+//     // document.write('<iframe width="1280" height="720" src="https://www.youtube.com/embed/NUXR4jT0NGU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+//     const videoSource = document.getElementById('videoSource');
+
+//     videoSource.setAttribute('src', 'video.mp4');
+// }
